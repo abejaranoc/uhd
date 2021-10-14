d231ffbbe0446f9a87426b348748a8ba309017be
Demba Komma <dbkomma@umich.edu>
2021-10-14 06:17:31 -0400

    preamble for pilot

--- fpga/dk_hdl/utils/preamble_detect.v
+++ fpga/dk_hdl/utils/preamble_detect.v
@@ -7,7 +7,7 @@ module  preamble_detect#(
   parameter [$clog2(MAX_LEN+1)-1:0] LEN = 4092,
   parameter [1:0] THRES_SEL = 2'b01,
   parameter NRX_TRIG        = 16, 
-  parameter [DATA_WIDTH-1:0] NOISE_POW = 50 
+  parameter [DATA_WIDTH-1:0] NOISE_POW = 15 
 )(
   input clk,
   input reset,
